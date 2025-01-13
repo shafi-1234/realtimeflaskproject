f298b87e1e81c2f73789b5ec662083269427a4ba
Mahammad shafi <mahammadshafishaik444@gmail.com>
2025-01-13 20:16:05 +0530

    gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,4 @@
-bind ="0.0.0.0:80"
-workers = 2
-gunicorn --timeout 120 myapp:app
+# gunicorn.conf.py
+timeout = 120  # Increase timeout to 120 seconds
+workers = 2   # Adjust workers based on CPU cores
+worker_class = 'gevent'  # Use async workers if applicable
