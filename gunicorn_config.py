9ad1f31c6bd6b2a831be0af4eeca6d1e196e6b64
Mahammad shafi <mahammadshafishaik444@gmail.com>
2025-01-13 13:48:52 +0530

    gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,2 @@
-bind = '0.0.0.0:8000'  # The address to bind to (adjust as needed)
-workers = 4  # Number of worker processes (adjust based on your system's capacity)
-accesslog = '-'  # Logs access to the console
-errorlog = '-'  # Logs errors to the console
-loglevel = 'info'  # Level of logging (debug, info, warning, etc.)
-timeout = 30  # Timeout for requests in seconds
-gunicorn --timeout 300 app:app
+bind ="0.0.0.0:80"
+workers = 2
