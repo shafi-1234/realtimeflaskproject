3411ea40556cd105f01d514af4bc10548317abf2
Mahammad shafi <mahammadshafishaik444@gmail.com>
2025-01-13 22:10:29 +0530

    gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,2 @@
-timeout = 120  # Increase timeout to 120 seconds
-workers = 2  # Set to 1 worker to minimize CPU usage
-worker_class = 'sync'  # Default synchronous workers to reduce overhead
-threads = 2  # Use 2 threads for handling concurrent requests within the same worker
+bind ="0.0.0.0:80"
+workers = 2
