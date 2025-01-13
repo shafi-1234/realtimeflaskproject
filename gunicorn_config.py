e9b68c693d011518e5aa1e99de38276b5edab552
Mahammad shafi <mahammadshafishaik444@gmail.com>
2025-01-13 15:19:31 +0530

    gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,5 +3,4 @@ workers = 4  # Number of worker processes (adjust based on your system's capacit
 accesslog = '-'  # Logs access to the console
 errorlog = '-'  # Logs errors to the console
 loglevel = 'info'  # Level of logging (debug, info, warning, etc.)
-timeout = 30  # Timeout for requests in seconds
---timeout
+timeout = 3  # Timeout for requests in seconds
